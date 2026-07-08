5d1703880c256f30ee1844a5ea228e607df08ae7
ruyana322 <156115962+ruyana322@users.noreply.github.com>
2026-07-08 17:49:05 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,20 @@ android {
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
-    defaultConfig {
+    defaultConfig {        ndk {
+            abiFilters += listOf("arm64-v8a", "x86_64")
+        }
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17"
+                arguments += listOf(
+                    "-DANDROID_STL=c++_shared",
+                    "-DANDROID_TOOLCHAIN=clang"
+                )
+            }
+        }
+
         applicationId = "com.d4nzxml.kythera"
         minSdk = 24
         targetSdk = 35
