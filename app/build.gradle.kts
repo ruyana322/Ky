ffd714d78eab1bd810b3db208348cb613188160a
ruyana322 <156115962+ruyana322@users.noreply.github.com>
2026-07-12 01:38:06 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ android {
 }
 
 dependencies {
+implementation("androidx.browser:browser:1.8.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
